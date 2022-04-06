77139553d1ea2a56818bc9f4eadd5313532d3ef4
bintanghr <haridaramadhan@gmail.com>
2022-04-06 09:34:17 +0700

    penambahan nested ternary

---
+++ Numbers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    internal class Numbers
+    {
+        int min, max;
+        public int FindMinimum(int number1, int number2)
+        {
+            min = (number1 < number2) ? number1 : number2;
+            return min;
+        }
+        public int FindMinimum(int number1, int number2, int number3)
+        {
+            //nested ternary
+            min = (number1 < number2) ?
+                /* true atau number1 lebih kecil
+                number1 dibandingkan lagi dengan number3
+                true -> min = number1 
+                false -> number3 lebih kecil -> number3 dibandingkan dengan number2
+                true -> min = number3 */
+                ( (number1 < number3) ? number1 : ((number3 < number2) ? number3 : number2) ) :
+                /* false atau number2 lebih kecil
+                number2 dibandingkan lagi dengan number3 
+                true -> min = number2
+                false -> number3 lebih kecil -> number3 dibandingkan dengan number1
+                true -> mmin = number 3 */
+                ( (number2 < number3) ? number2 : ((number3 < number1) ? number3 : number1) );
+            return min;
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            max = (number1 > number2) ? number1 : number2;
+            return max;
+        }
+        public int FindMaximum(int number1, int number2, int number3)
+        {
+            max = (number1 > number2) ?
+                ((number1 > number3) ? number1 : ((number3 > number2) ? number3 : number2)) :
+                ((number2 > number3) ? number2 : ((number3 > number1) ? number3 : number1));
+            return max;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers number = new Numbers();
+
+            Console.WriteLine("Find Minimum #1 = {0}", number.FindMinimum(6,7));
+            Console.WriteLine("Find Minimum #2 = {0}", number.FindMinimum(3, 7, 5));
+            Console.WriteLine();
+            Console.WriteLine("Find Maximum #1 = {0}", number.FindMaximum(4, 6));
+            Console.WriteLine("Find Maximum #2 = {0}", number.FindMaximum(8, 1, 7));
+
+            Console.ReadKey();
+        }
+    }
+}
